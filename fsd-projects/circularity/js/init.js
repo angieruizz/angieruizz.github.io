7967b7410f8daf94fcc0edbb6c7e0325b6e87477
angieruizz <angie.ruizmendoza@jpschools.org>
2025-06-23 14:10:17 +0000

    circles complete todos 1 and 2

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,15 +20,18 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+        var circle; // variable to hold a single circle when creating circles / iterating
+        var circles = []; // variable to store all circles in an array
 
         // TODO 2 : Create a function that draws a circle 
-        
-
-
+        function drawCircle() {
+        circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+        physikz.addRandomVelocity(circle, canvas, 5, 5);
+        view.addChild(circle);
+        circles.push(circle);
+} 
         // TODO 3 : Call the drawCircle() function
-
+        functionName();
 
 
         // TODO 7 : Use a loop to create multiple circles
